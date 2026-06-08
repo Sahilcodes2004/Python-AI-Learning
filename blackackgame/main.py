abf52c408c17fb75693f9e20cb84b47e554d1617
Sahil Jackson <sahiljackson86@gmail.com>
2026-06-08 23:57:32 +0530

    builing blackjackgame

--- blackackgame/main.py
+++ blackackgame/main.py
@@ -1,19 +1,39 @@
 import random
-cards=[]
-suits=["spaces","clubs","hearts","diamonds"]
-ranks=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-for suit in suits:
+
+class Deck:
+  def __init__(self):
+    
+   self.cards= []
+  suits=["spades","hearts","clubs","diamonds"]   
+  ranks=[
+    {"rank":"A","value":11},
+    {"rank":"2","value":2},
+    {"rank":"3","value":3},
+    {"rank":"4","value":4},
+    {"rank":"5","value":5},
+    {"rank":"6","value":6},
+    {"rank":"7","value":7},
+    {"rank":"8","value":8},
+    {"rank":"9","value":9},
+    {"rank":"10","value":10},
+    {"rank":"J","value":10},
+    {"rank":"Q","value":10},
+    {"rank":"K","value":10},
+   
+
+  ]
+  for suit in suits:
     for rank in ranks:
-
-     cards.append([suit,rank])   
-def shuffle( ):
- random.shuffle(cards)
-
-def deal():
-
- card=cards.pop()
- return card
-
-shuffle()
-card=deal()
-print(card)
+        self.cards.append([suit,rank])
+  def shuffle(self):
+    random.shuffle(self.cards) 
+  def deal(self,number):
+    cards_deal=[]
+    for x in range(number):
+     card=self.cards.pop()
+     cards_deal.append(card)
+     return cards_deal
+deck1=Deck()
+deck2=Deck()
+deck2.shuffle()
+print(deck1.cards)
