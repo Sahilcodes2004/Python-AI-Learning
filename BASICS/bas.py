5a17d8906763e52d68e936aaa1afc4f856abf886
Sahil Jackson <sahiljackson86@gmail.com>
2026-06-19 23:45:04 +0530

    learned strings basics:multiline strings,escape sequences,printing string chracters,multiline comment using

---
+++ BASICS/bas.py
@@ -0,0 +1,11 @@
+name='''sahil
+is a 
+good boy '''
+print(name)
+###string slicing uisng : operator
+name="shail is a girl"
+print(name[:9])
+aple=name[0:3]
+print(aple)
+mang0=name[-9:-1]
+print(mang0)
---
+++ BASICS/strings.py
@@ -0,0 +1,10 @@
+name="sahil"
+task='''1:clean the house
+2.solve 2 dsa questions'''
+artice='sahil is learning "a","a","the"'
+print(name)
+print(task)
+print(artice)
+for char in name:
+    print(char)
+    """leanred string basics"""
