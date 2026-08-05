a25f5905a9fadde416859418c965d6627530c0f0
Sahil Jackson <sahiljackson86@gmail.com>
2026-08-05 12:29:08 +0530

    Made a message encoder and decoder

--- BASICS/secretcode.py
+++ BASICS/secretcode.py
@@ -1,39 +1,39 @@
 import random
 import string
-s=input("Enter you secret message")
-x=len(s)
-b=""
-if(x<3):
-    dec=s[::-1]
+st=input("Enter the message")
+print("What you want:\n 1.CODING \n 2.DECODING ")
+coding=input("1 for coding and 0 for decoding").strip()
+coding=True if (coding=="1") else False
+print(coding)
+
+words=st.split(" ")
+
+if(coding):
+  nwords=[]
+  for word in words:
+    if(len(word)>=3):
+      t1="".join(random.choices(string.ascii_lowercase,k=3))
+      t2="".join(random.choices(string.ascii_lowercase,k=3))
+      stnew=t1+word[1:]+word[0]+t2
+      nwords.append(stnew)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
 else:
-    a=s[0]
-    b=s[1:]+a
-    
-    charpool=string.digits+string.ascii_letters
-    z=""
-    for i in range(4):
-        rand_char=random.choice(charpool)
-        z=z+rand_char
-    z=z+b+z
-    print("The Decoded Message is ",z)
-import random
-import string
+  nwords=[]
+  for word in words:
+    if(len(word)>=3):
+      trimmed=word[3:-3]
+      if(len(trimmed)>0):
+       stnew=trimmed[-1]+trimmed[:-1]
+      else:
+        trimmed=stnew
+      nwords.append(stnew)
+    else:
+      stnew=word[::-1]
+      nwords.append(stnew)
+  print(" ".join(nwords))
+  
+         
 
-s = input("Enter your secret message: ")
-x = len(s)
-
-if x < 3:
-    result = s[::-1]
-else:
-    a = s[0]
-    b = s[1:] + a
     
-    charpool = string.digits + string.ascii_letters
-    z = ""
-    for i in range(4):
-        rand_char = random.choice(charpool)
-        z += rand_char
-    
-    result = z + b + z
-
-print("The Encoded Message is:", result)   
